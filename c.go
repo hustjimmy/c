d624f26dbfe685cca0d36064638f09a055dd394f
zkf <jimgracy.hust@gmail.com>
2024-06-22 18:08:11 +0800

    fix

--- c.go
+++ c.go
@@ -7,7 +7,7 @@ import (
 )
 
 func CallC() {
-	fmt.Println("call C: v1.3.0")
+	fmt.Println("call C: v1.0.0")
 	fmt.Println("   --> call D:")
 	fmt.Printf("\t")
 	d.CallD()
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hustjimmy/c
+
+go 1.17
+
+require github.com/hustjimmy/d v1.2.3
